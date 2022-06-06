c7f529ab04285821a4b702c549192e403a027a00
Chantreck <vladnetaev@mail.ru>
2022-06-06 15:56:32 +0700

    Начал вёрстку экрана входа

--- app/src/main/java/com/chantreck/superduperquiz/AuthorizationActivity.kt
+++ app/src/main/java/com/chantreck/superduperquiz/AuthorizationActivity.kt
@@ -1,12 +1,21 @@
 package com.chantreck.superduperquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chantreck.superduperquiz.databinding.ActivityAuthorizationBinding
 
 class AuthorizationActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityAuthorizationBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_SuperDuperQuiz)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authorization)
+        setContentView(binding.root)
+
+        binding.authorizationSignInButton.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
