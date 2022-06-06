be73e4d3dc99ce961592105c4b8c1b0973ae77f3
Chantreck <vladnetaev@mail.ru>
2022-06-06 13:29:33 +0700

    Добавил splash screen
    Добавил тему Theme.SuperDuperQuiz.SplashScreen для splash screen
    
    Добавил в colors.xml все используемые цвета

--- app/src/main/java/com/chantreck/superduperquiz/AuthorizationActivity.kt
+++ app/src/main/java/com/chantreck/superduperquiz/AuthorizationActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class AuthorizationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_SuperDuperQuiz)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorization)
     }
