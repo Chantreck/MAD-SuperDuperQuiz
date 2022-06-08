43c18cf1e5e045e619dd856a6518616c247c26b4
Vladislav Netaev <vladnetaev@mail.ru>
2022-06-08 16:30:48 +0700

    Сверстал экран входа в систему
    Пока что не добавил валидацию полей для ввода
    Создал компоненты для поля ввода (view_input.xml) и toolbar (view_toolbar.xml)

--- app/src/main/java/com/chantreck/superduperquiz/SignInActivity.kt
+++ app/src/main/java/com/chantreck/superduperquiz/SignInActivity.kt
@@ -1,11 +1,35 @@
 package com.chantreck.superduperquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chantreck.superduperquiz.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private val binding by lazy { ActivitySignInBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(binding.root)
+
+        setViews()
+    }
+
+    private fun setViews() {
+        with (binding.signInToolbar.toolbar) {
+            setTitle(R.string.toolbar_sign_in)
+            setNavigationOnClickListener {
+                finish()
+            }
+        }
+
+        with(binding.nickname) {
+            label.setText(R.string.label_nickname)
+            editText.setHint(R.string.hint_nickname)
+        }
+
+        with(binding.password) {
+            label.setText(R.string.label_password)
+            editText.setHint(R.string.hint_password)
+        }
     }
 }
